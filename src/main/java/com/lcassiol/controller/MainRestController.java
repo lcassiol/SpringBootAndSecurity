14dc11525ae91d132233a87a8cbf80172403039c
Cassio Carvalho <lccassiol@gmail.com>
2018-01-06 14:47:02 -0200

    Change userRepository to userService, implement user-registration endpn

--- src/main/java/com/lcassiol/controller/MainRestController.java
+++ src/main/java/com/lcassiol/controller/MainRestController.java
@@ -1,16 +1,23 @@
 package com.lcassiol.controller;
 
+import com.lcassiol.IRepositories.IRoleRepository;
 import com.lcassiol.IRepositories.IUserRepository;
 import com.lcassiol.IServices.IAuthorService;
 import com.lcassiol.IServices.IBookService;
 import com.lcassiol.IServices.IUserService;
+import com.lcassiol.entities.Role;
 import com.lcassiol.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Controller
@@ -26,7 +33,7 @@ public class MainRestController {
     private IUserService userService;
 
     @Autowired
-    IUserRepository userRepository;
+    private IRoleRepository roleRepository;
 
 	@RequestMapping(method = RequestMethod.GET, path = "/entrar")
     public String entrar(Model model) {
@@ -44,7 +51,7 @@ public class MainRestController {
         model.addAttribute("books", bookService.getAll());
 
         System.out.println("Users here");
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.findAll();
         users.forEach(System.out::println);
 
         return "books";
@@ -61,5 +68,36 @@ public class MainRestController {
 	@RequestMapping(method = RequestMethod.GET, path = "/relatorio-equipe")
     public String relatorioEquipe() {
         return "relatorio-equipe";
+    }
+
+
+    @RequestMapping(value="/user-registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("user-registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/user-registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("user-registration");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully, click on top buttom for redirect to login page");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("user-registration");
+
+        }
+        return modelAndView;
     }
 }
