b0816d49513823213ea055a030eb97d189da07c4
Cassio Carvalho <lccassiol@gmail.com>
2018-01-07 21:44:04 -0200

    Implement method updateUser and change name findAll to getall :sparkles:

--- src/main/java/com/lcassiol/services/UserServiceImpl.java
+++ src/main/java/com/lcassiol/services/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service("userService")
 public class UserServiceImpl implements IUserService {
@@ -31,15 +32,33 @@ public class UserServiceImpl implements IUserService {
     @Override
     public void saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setActive(1);
+        user.setActive(true);
         Role userRole = roleRepository.findByRole("ROLE_BOOKS");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         userRepository.save(user);
     }
 
     @Override
-    public List<User> findAll() {
+    public List<User> getAll() {
         return userRepository.findAll();
     }
+
+    @Override
+    public User updateUser(User userAfterUpdate) {
+        User userBeforeUpdate = findUserByEmail(userAfterUpdate.getEmail());
+        Set<Role> roles = userAfterUpdate.getRoles();
+        userAfterUpdate.setRoles(userBeforeUpdate.getRoles());
+
+        if(userAfterUpdate.getPassword() != null){
+            userAfterUpdate.setPassword(userBeforeUpdate.getPassword());
+        }
+
+        if(roles != null){
+            userAfterUpdate.getRoles().addAll(roles);
+        }
+
+        return userRepository.saveAndFlush(userAfterUpdate);
+    }
+
 
 }
