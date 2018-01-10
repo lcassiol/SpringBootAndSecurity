dd7e5d895ba93465bead8aee76ad0b4135434961
Cassio Carvalho <lccassiol@gmail.com>
2018-01-09 22:09:45 -0200

    Implement bean to use h2 console webpage

--- src/main/java/com/lcassiol/config/UserDetailsSecurityConfig.java
+++ src/main/java/com/lcassiol/config/UserDetailsSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.lcassiol.config;
 
 import com.lcassiol.security.CustomUserDetailsService;
+import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,5 +19,12 @@ public class UserDetailsSecurityConfig {
         builder
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
     }
 }
